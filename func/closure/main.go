ebea292a60d8a77a157bfe01649b36664ac916e5
tatteredprince <tatteredprince@email.com>
2025-03-10 19:45:50 +0300

    add source for closures

---
+++ func/closure/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a, b := 0, 1
+
+	fibo := func() int { // anonymous function
+		a, b = b, a+b
+		return b
+	}
+
+	fmt.Println("a =", a, "b =", b)
+	fmt.Println(a, b, fibo(), fibo(), fibo(), a, b)
+	fmt.Println("a =", a, "b =", b)
+
+	fibo2 := func(a, b int) int { // anonymous function
+		a, b = b, a+b
+		return b
+	}
+
+	fmt.Println("fibo2:", fibo2(0, 1), fibo2(1, 1), fibo2(1, 2), fibo2(2, 3), fibo2(3, 5))
+
+	fibo3, fibo4 := mkFiboSeq(0, 1), mkFiboSeq(3, 5)
+	fmt.Println("fibo3:", fibo3(), fibo3(), fibo3(), fibo3(), fibo3())
+	fmt.Println("fibo4:", fibo4(), fibo4(), fibo4(), fibo4(), fibo4())
+}
+
+func mkFiboSeq(a, b int) func() int {
+	return func() int { // closure
+		a, b = b, a+b
+		return b
+	}
+}
